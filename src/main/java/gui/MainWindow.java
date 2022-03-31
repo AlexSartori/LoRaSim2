9031c371d5932281826455c88bb660023647a36c
Alex Sartori <alex.sartori1997@gmail.com>
2022-03-31 19:13:25 +0200

    Refactor MainWindow.java

--- src/main/java/gui/MainWindow.java
+++ src/main/java/gui/MainWindow.java
@@ -2,26 +2,17 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Random;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
-import lorasim2.LoRaGateway;
-import lorasim2.LoRaMarkovModel;
-import lorasim2.LoRaModelFactory;
-import lorasim2.LoRaNode;
-import lorasim2.SimConfig;
 import lorasim2.SimulationStats;
 import lorasim2.Simulator;
 
 /**
  * @author alex
  */
-public class MainWindow extends javax.swing.JFrame implements ActionListener {
+public class MainWindow extends javax.swing.JFrame {
     private Simulator sim;
     private JToolBar toolbar;
     private CanvasPanel canvas;
@@ -41,7 +32,9 @@ public class MainWindow extends javax.swing.JFrame implements ActionListener {
         
         /* Toolbar ---------------------------------------------------------- */
         JButton tb_newsim = new JButton("Run Simulation");
-        tb_newsim.addActionListener(this);
+        tb_newsim.addActionListener((ae) -> {
+            _createSimulation();
+        });
         
         JPanel tb_panel = new JPanel(new FlowLayout());
         tb_panel.add(tb_newsim);
@@ -61,20 +54,5 @@ public class MainWindow extends javax.swing.JFrame implements ActionListener {
         System.out.println("[Main]: Preparing results...");
         res_window = new ResultsWindow();
         res_window.showResults(res);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent ae) {
-        String cmd = ae.getActionCommand();
-        
-        switch (cmd) {
-            case "Run Simulation":
-                _createSimulation();
-                canvas.repaint();
-                break;
-            default:
-                System.err.println("[Main]: Unexpected action command in CanvasWindow listener: " + cmd);
-                break;
-        }
     }
 }
